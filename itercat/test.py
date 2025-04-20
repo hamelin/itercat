9015b53c5555370212453a661a0f5f985fd4e5d0
Benoit Hamelin <benoit@benoithamelin.com>
2025-04-20 11:48:59 -0400

    Project start

---
+++ itercat/test.py
@@ -0,0 +1,41 @@
+import pdb
+from types import TracebackType
+from typing import Protocol, Self
+
+
+class Reporter(Protocol):
+
+    def pass_test(self, tst: "test") -> None:
+        ...
+
+    def error_raised(self, tst: "test", value: Exception, tb: TracebackType) -> bool:
+        ...
+
+
+class ReporterMarimo:
+
+    def pass_test(self, tst: "test") -> None:
+        print(f"{tst.name}: ✅")
+
+    def error_raised(self, tst: "test", value: Exception, tb: TracebackType) -> bool:
+        if tst.debug:
+            pdb.post_mortem(tb)
+        return False
+
+
+class test:
+    reporter: Reporter = ReporterMarimo()
+
+    def __init__(self, name: str, debug: bool = False) -> None:
+        self.name = name
+        self.debug = debug
+
+    def __enter__(self) -> Self:
+        return self
+
+    def __exit__(self, typ: type, value: Exception, tb: TracebackType) -> bool:
+        if typ is None:
+            self.reporter.pass_test(self)
+            return False
+        else:
+            return self.reporter.error_raised(self, value, tb)
---
+++ test/hello.py
@@ -0,0 +1,39 @@
+
+
+import marimo
+
+__generated_with = "0.13.0"
+app = marimo.App(width="full")
+
+
+@app.cell
+def _():
+    import itercat as cat
+    from itercat.test import test
+    import marimo as mo
+    return cat, test
+
+
+@app.cell
+def _(cat, test):
+    with test("hello"):
+        assert cat.hello_world() == "Hello world"
+    return
+
+
+@app.cell
+def _(test):
+    with test("hey"):
+        assert "hey" == "hey"
+    return
+
+
+@app.cell
+def _(test):
+    with test("one", debug=True):
+        assert 1 == 2
+    return
+
+
+if __name__ == "__main__":
+    app.run()
